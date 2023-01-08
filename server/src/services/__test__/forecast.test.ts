74f3ae6b3817371709bbd1562f4f18f8650dd6b6
Joao Teles <joaojoseteles@gmail.com>
2023-01-08 12:17:22 -0300

    :white_check_mark: test-unit: forecast - retorno de arry com praias salvas, retorno array fazio das praias e erro de conexao com servico

--- server/src/services/__test__/forecast.test.ts
+++ server/src/services/__test__/forecast.test.ts
@@ -5,10 +5,11 @@ import { Beach, BeachPosition, Forecast } from "../forecast";
 jest.mock("@src/clients/stormGlass");
 
 describe("Forecast Service", () => {
+  const mockedStormGlassService = new StormGlass() as jest.Mocked<StormGlass>;
   it("should return the forecast for a list of beaches", async () => {
-    StormGlass.prototype.fetchPoints = jest
-      .fn()
-      .mockResolvedValue(stormGlassNormalizedResponseFixture);
+    mockedStormGlassService.fetchPoints.mockResolvedValue(
+      stormGlassNormalizedResponseFixture
+    );
 
     const beaches: Beach[] = [
       {
@@ -82,8 +83,32 @@ describe("Forecast Service", () => {
         ],
       },
     ];
-    const forecast = new Forecast(new StormGlass());
+    const forecast = new Forecast(mockedStormGlassService);
     const beachesWithRating = await forecast.processForecastForBeaches(beaches);
     expect(beachesWithRating).toEqual(expectedResponse);
+  });
+  it("should return an empty list when beaches array is empty", async () => {
+    const forecast = new Forecast();
+    const response = await forecast.processForecastForBeaches([]);
+    expect(response).toEqual([]);
+  });
+  it("should throw internal processing error when something goes wrong during the rating process", async () => {
+    const beaches: Beach[] = [
+      {
+        lat: -33.792726,
+        lng: 151.289824,
+        name: "Manly",
+        position: BeachPosition.E,
+        user: "some-id",
+      },
+    ];
+    mockedStormGlassService.fetchPoints.mockRejectedValue({
+      message: "Error fetching data",
+    });
+
+    const forecast = new Forecast(mockedStormGlassService);
+    await expect(forecast.processForecastForBeaches(beaches)).rejects.toThrow(
+      Error
+    );
   });
 });
